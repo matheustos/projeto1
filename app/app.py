8230196442ae66231c3edca926785062f48d9189
Matheus <matheusstos123456@gmail.com>
2023-04-24 19:35:34 -0300

    Correção de script

--- app/app.py
+++ app/app.py
@@ -8,11 +8,9 @@ app = Flask(__name__)
 @app.route('/cadastro', methods = ['POST'])
 def cadastro_user():
     dados = request.get_json()
-    # Criar hash de senha
-    hashed_senha = criar_hash_senha(dados['senha'])
 
     try:
-        models.criar(nome=dados['nome'], email=dados['email'], senha=hashed_senha, data_nascimento=dados['data_nascimento'], cpf=dados['cpf'], rg=dados['rg'])
+        models.criar(nome=dados['nome'], email=dados['email'], senha='teste', data_nascimento=dados['data_nascimento'], cpf=dados['cpf'], rg=dados['rg'])
         return jsonify({"message": "Usuário cadastrado com sucesso!"})
     except:
         return jsonify({"message": "Erro ao casdastrar usuário"})
@@ -26,8 +24,7 @@ def login():
         
     except:
         return jsonify({"message": "Email inválido!"})
-    else:
-        pass
+    
 
 
 if __name__ == '__main__':

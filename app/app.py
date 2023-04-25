8a857ebeac6322823a08a15bf5c7355a5d14c652
Matheus <matheusstos123456@gmail.com>
2023-04-24 21:25:51 -0300

    correção de bugs e falhas

--- app/app.py
+++ app/app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 @app.route('/cadastro', methods = ['POST'])
 def cadastro_user():
     dados = request.get_json()
-    hash_senha = sha256(dados['senha'].encode()) # hash de senha 
-    senha_armazenar = hash_senha.hexdigest # armazena hash no bd
+    senha = dados['senha']
+    hash_senha = sha256(senha.encode('utf-8')) # hash de senha 
+    senha_armazenar = hash_senha.hexdigest() # armazena hash no bd
 
     try:
         models.criar(nome=dados['nome'], email=dados['email'], senha=senha_armazenar, data_nascimento=dados['data_nascimento'], cpf=dados['cpf'], rg=dados['rg'])
@@ -20,17 +21,17 @@ def cadastro_user():
 @app.route('/login', methods = ['POST'])
 def login():
     dados_login = request.get_json()
-    hash_senha_login = sha256(dados_login['senha'].encode()).hexdigest # criptografa senha passada no login para comparar com a que está no bd
+    email = dados_login['email']
+    senha = dados_login['senha']
+    hash_senha_login = sha256(senha.encode('utf-8')).hexdigest() # criptografa senha passada no login para comparar com a que está no bd
   
-    resultado = models.checa_senha(parametro_json=dados_login['email'])   
-    
-    if len(resultado) == 0:
-        return jsonify({"message": "Email inválido!"})
+    resultado = models.checa_senha(parametro_json=email)   
+    senha_armazenada = resultado[0]
+
+    if hash_senha_login == senha_armazenada:
+        return jsonify({"message": "Senha válida"})
     else:
-        if hash_senha_login == resultado:
-            return jsonify({"message": "Senha válida"})
-        else:
-            return jsonify({"message": "Senha inválida"})
+        return jsonify({"message": "Senha inválida"})
     
 
 
